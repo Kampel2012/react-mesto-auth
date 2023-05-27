0199578c0f6c7ad33bcdcdfc880a0ae5417e4611
Anton Glazunov <kampel2012@yandex.ru>
2023-05-27 23:49:16 +0500

    замена браузер роутер на хешроутер

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-mesto-auth">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
